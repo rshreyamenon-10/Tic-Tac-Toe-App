92e4b9ff9e46a9a37d92aef9d77dc6cf92ba915a
rshreyamenon-10 <rshreyamenon@gmail.com>
2026-04-21 15:37:37 +0530

    Code for UC-5

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,35 @@
 public class TicTacToe {
 
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
     public static void main(String[] args) {
-        int slot = 7;
-        System.out.println("Slot: " + slot);
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        int testRow = 1;
+        int testCol = 1;
+
+        if (isValidMove(testRow, testCol)) {
+            System.out.println("Move accepted!");
+            board[testRow][testCol] = 'X'; 
+        } else {
+            System.out.println("Move rejected!");
+        }
     }
 
-    static int getRowFromSlot(int slot) {
-         return (slot - 1) / 3;
-    }
-
-    static int getColFromSlot(int slot) {
-         return (slot - 1) % 3;
+    static boolean isValidMove(int row, int col) {
+        if (row >= 0 && row <= 2 && col >= 0 && col <= 2) {
+            if (board[row][col] == '-') {
+                return true; 
+            } else {
+                System.out.println("Error: Cell is already occupied.");
+                return false;
+            }
+            
+        } else {
+            System.out.println("Error: Move is out of bounds (must be 0-2).");
+            return false;
+        }
     }
 }
