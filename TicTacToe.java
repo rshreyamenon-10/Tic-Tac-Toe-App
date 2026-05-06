83f1474d39ed1b94ceba77acc114f3d3c41ae977
rshreyamenon-10 <rshreyamenon@gmail.com>
2026-05-06 15:55:35 +0530

    Code for UC-8

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,60 +1,46 @@
-import java.util.Random;
-
 public class TicTacToe {
 
-    static char[][] board = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
-    };
-    static char computerSymbol = 'O';
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
 
     public static void main(String[] args) {
-        System.out.println("Board before computer move:");
-        printBoard();
         
-        computerMove();
-        
-        System.out.println("\nBoard after computer move:");
-        printBoard();
+        while (!gameOver) {
+            if (isHumanTurn) {
+                humanMove();
+            } else {
+                computerMove();
+            }
+
+            if (checkWin() || checkDraw()) {
+                gameOver = true;
+                System.out.println("Game Over!");
+            } else {
+                isHumanTurn = !isHumanTurn;
+            }
+        }
+    }
+
+    static void humanMove() {
+        System.out.println("Human player's turn.");
     }
 
     static void computerMove() {
-        Random random = new Random();
-        boolean validMoveFound = false;
+        System.out.println("Computer player's turn.");
+    }
 
-        while (!validMoveFound) {
-            int slot = random.nextInt(9) + 1;
-            
-            int row = getRow(slot);
-            int col = getCol(slot);
+    static boolean checkWin() {
+        return false; 
+    }
 
-            if (isValidMove(row, col)) {
-                board[row][col] = computerSymbol;
-                validMoveFound = true;
-                System.out.println("Computer selected valid slot: " + slot);
-            }
+    static int simulatedTurns = 0;
+    
+    static boolean checkDraw() {
+        simulatedTurns++;
+        if (simulatedTurns >= 3) {
+            System.out.println("Draw detected.");
+            return true;
         }
-    }
-
-    static int getRow(int slot) {
-        return (slot - 1) / 3;
-    }
-
-    static int getCol(int slot) {
-        return (slot - 1) % 3;
-    }
-
-    static boolean isValidMove(int row, int col) {
-        return board[row][col] == '-';
-    }
-
-    static void printBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+        return false;
     }
 }
